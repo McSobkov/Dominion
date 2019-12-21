b47dd075079987967ecd56354b89c84111ceaae9
McSobkov <sobkovmax@gmail.com>
2019-12-21 13:57:02 -0500

    Common actions cards take
    Acts as an adapter of sorts

---
+++ src/main/java/ActionHelper.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public final class ActionHelper {
+    private ActionHelper(){}
+
+    /**
+     *
+     * @param trash pile
+     * @param card being trashed
+     * @return success
+     */
+    public static boolean trashCard(ArrayList<Card> trash, Card card){
+        if(card == null) return false;
+        trash.add(card);
+        return true;
+    }
+
+    public static void drawCard(Player p, int count){
+        p.drawCards(count);
+    }
+
+}
