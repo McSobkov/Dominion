f5b5ccbc374fca74934beafb23cf8c5017d1ae84
McSobkov <sobkovmax@gmail.com>
2019-12-21 22:43:22 -0500

    Deprecated

--- src/main/java/CardActionUpTo.java
+++
@@ -1,7 +0,0 @@
-/**
- * For card actions that do not have a definite limit on number of cards affected
- */
-@SuppressWarnings("checkstyle:InterfaceIsType")
-public interface CardActionUpTo extends CardAction {
-    boolean DONE = false;
-}
